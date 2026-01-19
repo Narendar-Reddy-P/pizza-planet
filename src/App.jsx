47b90dcff7ef958c09a27ef1f27444fd9ba38cb1
narendar <narendar@gmail.com>
2026-01-19 10:23:24 +0530

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ function App() {
 function Pizza(props) {
   return (
     <div className="pizza">
-      <img src={`../public/pizzas/${props.id}.jpg`} alt={props.name} />
+      <img
+        src={`/pizza-planet/public/pizzas/${props.id}.jpg`}
+        alt={props.name}
+      />
       <div>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
