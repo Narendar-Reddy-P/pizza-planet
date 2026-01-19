6d6fda8a5fbad70634cbbba4648a814d93673c2b
narendar <narendar@gmail.com>
2026-01-19 09:27:46 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import pizzaData from "./assets/data";
+import "../src/index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={`../public/pizzas/${props.id}.jpg`} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{`₹ ${props.price}`}</span>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pizza Planet</h1>;
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+
+      {pizzaData.map((pizzaData) => {
+        return (
+          <Pizza
+            name={pizzaData.name}
+            ingredients={pizzaData.ingredients}
+            price={pizzaData.price}
+            id={pizzaData.id}
+          />
+        );
+      })}
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  return (
+    <footer className="footer">
+      {hour >= openHour && hour <= closeHour
+        ? "We're currently open"
+        : "Sorry, we're closed for now"}
+    </footer>
+  );
+}
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,52 @@
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 600,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+    id: "focaccia",
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 1000,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+    id: "margherita",
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 1200,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+    id: "spinaci",
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 1200,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+    id: "funghi",
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 1500,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+    id: "salamino",
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 1800,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+    id: "prosciutto",
+  },
+];
+
+export default pizzaData;
